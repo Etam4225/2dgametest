64fbce4d36ba234376c8d174e2681733307b5d42
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-03-01 15:22:28 -0500

    files so big tho

---
+++ bruh/Assets/rigid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigid : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float axisx = Input.GetAxis("Horizontal");
+        float axisy = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(axisx, axisy) * Time.deltaTime * speed);
+    }
+}
